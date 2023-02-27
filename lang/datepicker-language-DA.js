0f7e378bf45c5042562c5007b16462c045d7207c
Noah Raskin <noahgraskin@gmail.com>
2023-02-26 22:15:38 -0500

    Initial Commit v0.1.0
    Initial commit for Date Picker

---
+++ lang/datepicker-language-DA.js
@@ -0,0 +1,1 @@
+dateDropperSetup.languages["da"] = {"name":"Dansk","m":{"s":["jan","feb","mar","apr","maj","jun","jul","aug","sep","okt","nov","dec"],"f":["januar","februar","marts","april","maj","juni","juli","august","september","oktober","november","december"]},"w":{"s":["s","m","t","o","t","f","l"],"f":["s\u00f8ndag","mandag","tirsdag","onsdag","torsdag","fredag","l\u00f8rdag"]}}
---
+++ lang/datepicker-language-NL.js
@@ -0,0 +1,1 @@
+dateDropperSetup.languages["nl"] = {"name":"Nederlands","m":{"s":["jan","feb","maa","apr","mei","jun","jul","aug","sep","okt","nov","dec"],"f":["januari","februari","maart","april","mei","juni","juli","augustus","september","oktober","november","december"]},"w":{"s":["z","m","d","w","d","v","z"],"f":["zondag","maandag","dinsdag","woensdag","donderdag","vrijdag","zaterdag"]}}
